50c5d65ef0302be55aeb72a18ce1f4f70772e602
Huy Pham <huy.phamkeawy@hcmut.edu.vn>
2026-09-01 13:07:23 +0700

    Create more enemies

---
+++ Assets/Entities/Enemy/Zombie/Zombie.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Zombie : Enemy
+{
+    [SerializeField] private MovementComponent movementComponent;
+    [SerializeField] private LifeComponent lifeComponent;
+
+
+
+    private void OnEnable()
+    {
+        lifeComponent.Died += Die;
+    }
+
+
+    private void OnDisable()
+    {
+        lifeComponent.Died -= Die;
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+
+
+    public override void OnSpawn()
+    {
+        base.OnSpawn();
+    }
+
+
+    public override void OnDespawn()
+    {
+        base.OnDespawn();
+    }
+
+
+    protected override void Die()
+    {
+        base.Die();
+    }
+}
