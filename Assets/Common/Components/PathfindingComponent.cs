b91bfc92b3e8094a20f266408e8e51e736368834
Huy Pham <huy.phamkeawy@hcmut.edu.vn>
2026-09-01 11:54:33 +0700

    Implement PathfindingComponent:

--- Assets/Common/Components/PathfindingComponent.cs
+++ Assets/Common/Components/PathfindingComponent.cs
@@ -1,15 +1,47 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
+// I only implement "Run straight to player" AI so this is enough
 public class PathfindingComponent : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private StatComponent statComponent;
+    private Stat movementStat;
+    private float currentSpeed = 0.0f;
+    public void SetSpeed(float newSpeed)
     {
-        
+        currentSpeed = newSpeed;
     }
 
 
-    void Update()
+
+    private void Start()
     {
-        
+        agent.updateRotation = false;
+		agent.updateUpAxis = false;
+
+        movementStat = statComponent.GetStat(StatType.MOVEMENT_SPEED);
+        currentSpeed = movementStat.GetCurrentValue();
+        SubscribeToMovementSpeedChanged(UpdateSpeedAfterChanged);
+    }
+
+
+    private void Update()
+    {
+        agent.speed = currentSpeed;
+        agent.SetDestination(GameManager.Instance.playerManager.currentPlayer.transform.position);
+    }
+
+
+    private void UpdateSpeedAfterChanged(float oldValue, float currentValue, float maxValue)
+    {
+        SetSpeed(currentValue);
+    }
+
+
+    private void SubscribeToMovementSpeedChanged(Action<float, float, float> listener) 
+    {
+        statComponent.SubscribeToStat(StatType.MOVEMENT_SPEED, listener);
     }
 }
