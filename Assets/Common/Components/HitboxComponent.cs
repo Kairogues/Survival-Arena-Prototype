72ebc697f88d2120ad26b709af8e85b29514d221
Huy Pham <huy.phamkeawy@hcmut.edu.vn>
2026-09-04 09:42:26 +0700

    Implement local i-frame mechanic

--- Assets/Common/Components/HitboxComponent.cs
+++ Assets/Common/Components/HitboxComponent.cs
@@ -1,16 +1,20 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class HitboxComponent : MonoBehaviour
 {
+    [SerializeField] private float SECONDS_PER_DAMAGE_TICK = 0.25f;
     public event Action<HurtboxComponent> HitHurtbox;
+    public event Action<HurtboxComponent> HurtboxExit;
     public event Action HitObstacle;
+    private Dictionary<HurtboxComponent, float> nextHitTimes = new();
     [SerializeField] private StatComponent statComponent;
 
 
 
-    private void OnTriggerEnter2D(Collider2D hurtboxInfo)
+    private void OnTriggerStay2D(Collider2D hurtboxInfo)
     {
         int obstacleLayer = LayerMask.NameToLayer("Obstacle");
         if (hurtboxInfo.gameObject.layer == obstacleLayer)
@@ -19,12 +23,30 @@ public class HitboxComponent : MonoBehaviour
             return;
         }
 
-        HurtboxComponent hurtbox = hurtboxInfo.GetComponent<HurtboxComponent>();
-
-        if (hurtbox != null)
+        if (!hurtboxInfo.TryGetComponent(out HurtboxComponent hurtbox)) 
         {
-            hurtbox.TakeDamge(GetDamageAmount());
-            RegisterHurtboxHit(hurtbox);
+            return;
+        }
+
+        if (nextHitTimes.TryGetValue(hurtbox, out float nextValidTime))
+        {
+            if (Time.time < nextValidTime) 
+            {
+                return;
+            }
+        }
+
+        hurtbox.TakeDamge(GetDamageAmount());
+        nextHitTimes[hurtbox] = Time.time + SECONDS_PER_DAMAGE_TICK;
+
+        RegisterHurtboxHit(hurtbox);
+    }
+
+    private void OnTriggerExit2D(Collider2D hurtboxInfo)
+    {
+        if (hurtboxInfo.TryGetComponent(out HurtboxComponent hurtbox))
+        {
+            nextHitTimes.Remove(hurtbox);
         }
     }
 
